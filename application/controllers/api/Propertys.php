30f137823b5b8e5c0af3a206f1e3b80f5dee8ef8
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-05-03 21:34:56 -0300

    akjasdh

--- application/controllers/api/Propertys.php
+++ application/controllers/api/Propertys.php
@@ -8,176 +8,174 @@ use Restserver\Libraries\REST_Controller;
 class Propertys extends REST_Controller
 {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->library('Authorization_Token');
-		$this->load->model('user_model');
-		$this->load->model('email_model');
-		$this->load->model('broker_model');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('Authorization_Token');
+        $this->load->model('user_model');
+        $this->load->model('email_model');
+        $this->load->model('broker_model');
+    }
 
 
 
-	public function broker_propriety_post()
-	{
-		$this->form_validation->set_rules('user_id', 'User ID', 'trim|required');
+    public function broker_propriety_post()
+    {
+        $this->form_validation->set_rules('user_id', 'User ID', 'trim|required');
 
-		if ($this->form_validation->run() == false) {
+        if ($this->form_validation->run() == false) {
 
-			$final['status'] = false;
-			$final['message'] = validation_errors();
-			$final['note'] = 'Erro no formulário.';
+            $final['status'] = false;
+            $final['message'] = validation_errors();
+            $final['note'] = 'Erro no formulário.';
 
-			$this->response($final, REST_Controller::HTTP_OK);
-		} else {
+            $this->response($final, REST_Controller::HTTP_OK);
+        } else {
 
-			$headers = $this->input->request_headers();
+            $headers = $this->input->request_headers();
 
-			if (isset($headers['Authorization'])) {
+            if (isset($headers['Authorization'])) {
 
-				$decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+                $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
 
-				if ($decodedToken['status']) {
+                if ($decodedToken['status']) {
 
-					$user_id = $this->input->post('user_id');
-					$_broker_propertys =  $this->broker_model->get_broker_propertys($user_id);
+                    $user_id = $this->input->post('user_id');
+                    $_broker_propertys =  $this->broker_model->get_broker_propertys($user_id);
 
-					if ($_broker_propertys) {
+                    if ($_broker_propertys) {
 
-						$final['status'] = true;
-						$final['message'] = 'Imóveis encontradas com sucesso.';
-						$final['response'] = $_broker_propertys;
-						$final['note'] = 'Dados   encontrados get_broker_propertys()';
+                        $final['status'] = true;
+                        $final['message'] = 'Imóveis encontradas com sucesso.';
+                        $final['response'] = $_broker_propertys;
+                        $final['note'] = 'Dados   encontrados get_broker_propertys()';
 
-						$this->response($final, REST_Controller::HTTP_OK);
-					} else {
+                        $this->response($final, REST_Controller::HTTP_OK);
+                    } else {
 
-						$final['status'] = false;
-						$final['message'] = 'Nenhum imoveil encontrado.';
-						$final['note'] = 'Erro em get_broker_propertys()';
+                        $final['status'] = false;
+                        $final['message'] = 'Nenhum imoveil encontrado.';
+                        $final['note'] = 'Erro em get_broker_propertys()';
 
-						$this->response($final, REST_Controller::HTTP_OK);
-					}
+                        $this->response($final, REST_Controller::HTTP_OK);
+                    }
+                } else {
 
-				} else {
+                    $final['status'] = false;
+                    $final['message'] = 'Sua sessão expirou.';
+                    $final['note'] = 'Erro em $decodedToken["status"]';
+                    $this->response($decodedToken);
+                }
+            } else {
 
-					$final['status'] = false;
-					$final['message'] = 'Sua sessão expirou.';
-					$final['note'] = 'Erro em $decodedToken["status"]';
-					$this->response($decodedToken);
-				}
-			} else {
+                $final['status'] = false;
+                $final['message'] = 'Falha na autenticação.';
+                $final['note'] = 'Erro em validateToken()';
 
-				$final['status'] = false;
-				$final['message'] = 'Falha na autenticação.';
-				$final['note'] = 'Erro em validateToken()';
-
-				$this->response($final, REST_Controller::HTTP_OK);
-			}
-		}
-	}
+                $this->response($final, REST_Controller::HTTP_OK);
+            }
+        }
+    }
 
     public function search_broker_propertys_post()
-	{
-		$this->form_validation->set_rules('user_id', 'User ID', 'trim|required');
-		$this->form_validation->set_rules('query', 'User ID', 'trim|required');
+    {
+        $this->form_validation->set_rules('user_id', 'User ID', 'trim|required');
+        $this->form_validation->set_rules('query', 'User ID', 'trim|required');
 
-		if ($this->form_validation->run() == false) {
+        if ($this->form_validation->run() == false) {
 
-			$final['status'] = false;
-			$final['message'] = validation_errors();
-			$final['note'] = 'Erro no formulário.';
+            $final['status'] = false;
+            $final['message'] = validation_errors();
+            $final['note'] = 'Erro no formulário.';
 
-			$this->response($final, REST_Controller::HTTP_OK);
-		} else {
+            $this->response($final, REST_Controller::HTTP_OK);
+        } else {
 
-			$headers = $this->input->request_headers();
+            $headers = $this->input->request_headers();
 
-			if (isset($headers['Authorization'])) {
+            if (isset($headers['Authorization'])) {
 
-				$decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+                $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
 
-				if ($decodedToken['status']) {
+                if ($decodedToken['status']) {
 
-					$user_id = $this->input->post('user_id');
+                    $user_id = $this->input->post('user_id');
                     $query = $this->input->post('query');
 
-					$_broker_propertys =  $this->broker_model->search_broker_propertys($user_id, $query);
+                    $_broker_propertys =  $this->broker_model->search_broker_propertys($user_id, $query);
 
-					if ($_broker_propertys) {
+                    if ($_broker_propertys) {
 
-						$final['status'] = true;
-						$final['message'] = 'Imóveis encontradas com sucesso.';
-						$final['response'] = $_broker_propertys;
-						$final['note'] = 'Dados   encontrados get_broker_propertys()';
+                        $final['status'] = true;
+                        $final['message'] = 'Imóveis encontradas com sucesso.';
+                        $final['response'] = $_broker_propertys;
+                        $final['note'] = 'Dados   encontrados get_broker_propertys()';
 
-						$this->response($final, REST_Controller::HTTP_OK);
-					} else {
+                        $this->response($final, REST_Controller::HTTP_OK);
+                    } else {
 
-						$final['status'] = false;
-						$final['message'] = 'Nenhum imoveil encontrado.';
-						$final['note'] = 'Erro em get_broker_propertys()';
+                        $final['status'] = false;
+                        $final['message'] = 'Nenhum imoveil encontrado.';
+                        $final['note'] = 'Erro em get_broker_propertys()';
 
-						$this->response($final, REST_Controller::HTTP_OK);
-					}
+                        $this->response($final, REST_Controller::HTTP_OK);
+                    }
+                } else {
 
-				} else {
+                    $final['status'] = false;
+                    $final['message'] = 'Sua sessão expirou.';
+                    $final['note'] = 'Erro em $decodedToken["status"]';
+                    $this->response($decodedToken);
+                }
+            } else {
 
-					$final['status'] = false;
-					$final['message'] = 'Sua sessão expirou.';
-					$final['note'] = 'Erro em $decodedToken["status"]';
-					$this->response($decodedToken);
-				}
-			} else {
+                $final['status'] = false;
+                $final['message'] = 'Falha na autenticação.';
+                $final['note'] = 'Erro em validateToken()';
 
-				$final['status'] = false;
-				$final['message'] = 'Falha na autenticação.';
-				$final['note'] = 'Erro em validateToken()';
+                $this->response($final, REST_Controller::HTTP_OK);
+            }
+        }
+    }
 
-				$this->response($final, REST_Controller::HTTP_OK);
-			}
-		}
-	}
-
-    public function add_broker_property_post() {
+    public function add_broker_property_post()
+    {
 
         $this->form_validation->set_rules('property_user_id', 'User ID', 'trim|required');
-		$this->form_validation->set_rules('property_title', 'Título do Imóvel', 'trim|required');
-		$this->form_validation->set_rules('proprerty_type', 'Tipo do Imóvel', 'trim|required');
-		$this->form_validation->set_rules('property_type_offer', 'Tipo de Oferta do Imóvel', 'trim|required');
-		$this->form_validation->set_rules('property_price', 'Preço do Imóvel', 'trim|required');
-		$this->form_validation->set_rules('property_area', 'Área do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('property_title', 'Título do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('proprerty_type', 'Tipo do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('property_type_offer', 'Tipo de Oferta do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('property_price', 'Preço do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('property_area', 'Área do Imóvel', 'trim|required');
 
-		$this->form_validation->set_rules('property_function', 'Função do Imóvel', 'trim|required');
-		$this->form_validation->set_rules('property_disponibility', 'Disponibilidade do Imóvel', 'trim|required');
-		$this->form_validation->set_rules('property_exclusive', 'Exclusividade do Imóvel', 'trim|required');
-        
-		$this->form_validation->set_rules('property_condominio', 'Valor do Condominio do Imóvel', 'numeric');
-		$this->form_validation->set_rules('property_iptu', 'IPTU do Imóvel', 'numeric');
-		$this->form_validation->set_rules('property_room', 'Qtd de Quartos do Imóvel', 'integer');
-		$this->form_validation->set_rules('property_bathroom', 'Qtd de Banheiros do Imóvel', 'integer');
-		$this->form_validation->set_rules('property_places', 'Qtd de cômodos do Imóvel', 'integer');
+        $this->form_validation->set_rules('property_function', 'Função do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('property_disponibility', 'Disponibilidade do Imóvel', 'trim|required');
+        $this->form_validation->set_rules('property_exclusive', 'Exclusividade do Imóvel', 'trim|required');
 
-		if ($this->form_validation->run() == false) {
+        $this->form_validation->set_rules('property_condominio', 'Valor do Condominio do Imóvel', 'numeric');
+        $this->form_validation->set_rules('property_iptu', 'IPTU do Imóvel', 'numeric');
+        $this->form_validation->set_rules('property_room', 'Qtd de Quartos do Imóvel', 'integer');
+        $this->form_validation->set_rules('property_bathroom', 'Qtd de Banheiros do Imóvel', 'integer');
+        $this->form_validation->set_rules('property_places', 'Qtd de cômodos do Imóvel', 'integer');
 
-			$final['status'] = false;
-			$final['message'] = validation_errors();
-			$final['note'] = 'Erro no formulário.';
+        if ($this->form_validation->run() == false) {
 
-			$this->response($final, REST_Controller::HTTP_OK);
+            $final['status'] = false;
+            $final['message'] = validation_errors();
+            $final['note'] = 'Erro no formulário.';
 
-		} else {
+            $this->response($final, REST_Controller::HTTP_OK);
+        } else {
 
-			$headers = $this->input->request_headers();
+            $headers = $this->input->request_headers();
 
-			if (isset($headers['Authorization'])) {
+            if (isset($headers['Authorization'])) {
 
-				$decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+                $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
 
-				if ($decodedToken['status']) {
+                if ($decodedToken['status']) {
 
-					$data['property_user_id'] = $this->input->post('property_user_id');
+                    $data['property_user_id'] = $this->input->post('property_user_id');
                     $data['property_title'] = $this->input->post('property_title');
                     $data['proprerty_type'] = $this->input->post('proprerty_type');
                     $data['property_type_offer'] = $this->input->post('property_type_offer');
@@ -195,45 +193,61 @@ class Propertys extends REST_Controller
 
                     $data['property_main_image'] = $this->input->post('property_main_image');
 
+                    $property_main_image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $data['property_main_image']));
+
+                    // Define o caminho onde a imagem será salva
+                    $path = 'assets/images/property/';
+
+                    // Gera um nome único para a imagem
+                    $file_name = uniqid() . '.jpg';
+
+                    // Caminho completo da imagem
+                    $file_path = $path . $file_name;
+
+                    // Salva a imagem no servidor
+                    if (file_put_contents($file_path, $property_main_image)) {
+                        echo 'Imagem salva com sucesso em: ' . $file_path;
+                    } else {
+                        echo 'Erro ao salvar a imagem.';
+                    }
+
 
                     print_r($data);
 
-					// $_broker_propertys =  $this->broker_model->search_broker_propertys($user_id, $query);
+                    // $_broker_propertys =  $this->broker_model->search_broker_propertys($user_id, $query);
 
-					// if ($_broker_propertys) {
+                    // if ($_broker_propertys) {
 
-						$final['status'] = true;
-						$final['message'] = 'Imóveil adicionado com sucesso.';
-						$final['response'] = $data;
-						$final['note'] = 'Dados   encontrados add_broker_property()';
+                    $final['status'] = true;
+                    $final['message'] = 'Imóveil adicionado com sucesso.';
+                    $final['response'] = $data;
+                    $final['note'] = 'Dados   encontrados add_broker_property()';
 
-					// 	$this->response($final, REST_Controller::HTTP_OK);
-					// } else {
+                    // 	$this->response($final, REST_Controller::HTTP_OK);
+                    // } else {
 
-					// 	$final['status'] = false;
-					// 	$final['message'] = 'Nenhum imoveil encontrado.';
-					// 	$final['note'] = 'Erro em get_broker_propertys()';
+                    // 	$final['status'] = false;
+                    // 	$final['message'] = 'Nenhum imoveil encontrado.';
+                    // 	$final['note'] = 'Erro em get_broker_propertys()';
 
-					// 	$this->response($final, REST_Controller::HTTP_OK);
-					// }
+                    // 	$this->response($final, REST_Controller::HTTP_OK);
+                    // }
 
-				} else {
+                } else {
 
-					$final['status'] = false;
-					$final['message'] = 'Sua sessão expirou.';
-					$final['note'] = 'Erro em $decodedToken["status"]';
-					$this->response($decodedToken);
-				}
-			} else {
+                    $final['status'] = false;
+                    $final['message'] = 'Sua sessão expirou.';
+                    $final['note'] = 'Erro em $decodedToken["status"]';
+                    $this->response($decodedToken);
+                }
+            } else {
 
-				$final['status'] = false;
-				$final['message'] = 'Falha na autenticação.';
-				$final['note'] = 'Erro em validateToken()';
+                $final['status'] = false;
+                $final['message'] = 'Falha na autenticação.';
+                $final['note'] = 'Erro em validateToken()';
 
-				$this->response($final, REST_Controller::HTTP_OK);
-			}
-		}
+                $this->response($final, REST_Controller::HTTP_OK);
+            }
+        }
     }
-
-	
 }
