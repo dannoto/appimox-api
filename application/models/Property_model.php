e28502b580da0f473cab8cbb6eae2709fac12d44
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-05-13 16:10:02 -0300

    .

--- application/models/Property_model.php
+++ application/models/Property_model.php
@@ -13,7 +13,7 @@ class Property_model extends CI_Model
         $this->load->database();
     }
 
-    public function get_propertys($property_id)
+    public function get_property($property_id)
     {
         $this->db->where('id', $property_id);
         $this->db->where('is_deleted', 0);
