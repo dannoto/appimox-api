d095f1d838616393ebb09b6d934d3d94fbe1e62d
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-05-13 15:58:27 -0300

    .

--- application/config/routes.php
+++ application/config/routes.php
@@ -88,6 +88,8 @@ $route['get_broker_property_home'] = 'api/Propertys/get_broker_property_home';
 
 // Locations
 $route['get_locations'] = 'api/Locations/get_locations';
+$route['get_propertys_by_range'] = 'api/Propertys/get_propertys_by_range';
+
 
 // Locations
 
--- application/controllers/api/Propertys.php
+++ application/controllers/api/Propertys.php
@@ -694,4 +694,72 @@ class Propertys extends REST_Controller
         }
 
     }
+
+
+    // 
+
+    public function get_propertys_by_range() {
+
+        $this->form_validation->set_rules('user_id', 'User ID', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+
+            $final['status'] = false;
+            $final['message'] = validation_errors();
+            $final['note'] = 'Erro no formulário.';
+
+            $this->response($final, REST_Controller::HTTP_OK);
+        } else {
+
+            $headers = $this->input->request_headers();
+
+            if (isset($headers['Authorization'])) {
+
+                $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+
+                if ($decodedToken['status']) {
+
+                    $markers_data = json_decode($this->input->post('markers_data'));
+
+                    print_r($markers_data);
+
+                    // $_broker_propertys =  $this->broker_model->search_broker_propertys_home($property_user_id, $filter);
+
+                    // if ($_broker_propertys) {
+
+                    //     $final['status'] = true;
+                    //     $final['message'] = 'Imóveis encontradas com sucesso.';
+                    //     $final['response'] = $_broker_propertys;
+                    //     $final['note'] = 'Dados   encontrados search_broker_propertys_home()';
+
+                    //     $this->response($final, REST_Controller::HTTP_OK);
+                    // } else {
+
+                    //     $final['status'] = false;
+                    //     $final['message'] = 'Nenhum imovel encontrado.';
+                    //     $final['response'] = $_broker_propertys;
+
+                    //     $final['note'] = 'Erro em search_broker_propertys_home()';
+
+                    //     $this->response($final, REST_Controller::HTTP_OK);
+                    // }
+
+                } else {
+
+                    $final['status'] = false;
+                    $final['message'] = 'Sua sessão expirou.';
+                    $final['note'] = 'Erro em $decodedToken["status"]';
+                    $this->response($decodedToken);
+                }
+            } else {
+
+                $final['status'] = false;
+                $final['message'] = 'Falha na autenticação.';
+                $final['note'] = 'Erro em validateToken()';
+
+                $this->response($final, REST_Controller::HTTP_OK);
+            }
+        }
+
+    }
 }
