37e5ccc3e1fcccc8c4e7cb6d35ff7bc5f1bb0f48
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-05-23 16:39:44 -0300

    .

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -1480,33 +1480,33 @@ class User extends REST_Controller
 
 					if ($user_data) {
 
-						// $property_estado = $this->property_model->suggest_property_by_estado($user_data->user_state);
-						// $property_cidade = $this->property_model->suggest_property_by_cidade($user_data->user_city);
+						$property_estado = $this->property_model->suggest_property_by_estado($user_data->user_state);
+						$property_cidade = $this->property_model->suggest_property_by_cidade($user_data->user_city);
 
 
-						// if (count($property_cidade) > 0) {
+						if (count($property_cidade) > 0) {
 
-						// 	$final['status'] = true;
-						// 	$final['message'] = 'Sugestao por cidade encontrados com sucesso';
-						// 	$final['response'] = $property_cidade;
-						// 	$final['note'] = 'Dados encontrados suggest_property_by_cidade()';
-						// 	$this->response($final, REST_Controller::HTTP_OK);
+							$final['status'] = true;
+							$final['message'] = 'Sugestao por cidade encontrados com sucesso';
+							$final['response'] = $property_cidade;
+							$final['note'] = 'Dados encontrados suggest_property_by_cidade()';
+							$this->response($final, REST_Controller::HTTP_OK);
 
-						// } else if (count($property_estado) > 0) {
+						} else if (count($property_estado) > 0) {
 
-						// 	$final['status'] = true;
-						// 	$final['message'] = 'Sugestao por estado encontrados com sucesso';
-						// 	$final['response'] = $property_estado;
-						// 	$final['note'] = 'Dados encontrados suggest_property_by_estado()';
-						// 	$this->response($final, REST_Controller::HTTP_OK);
+							$final['status'] = true;
+							$final['message'] = 'Sugestao por estado encontrados com sucesso';
+							$final['response'] = $property_estado;
+							$final['note'] = 'Dados encontrados suggest_property_by_estado()';
+							$this->response($final, REST_Controller::HTTP_OK);
 
-						// } else {
+						} else {
 
 							$final['status'] = false;
-							$final['message'] = $this->property_model->suggest_property_by_estado($user_data->user_state);
+							$final['message'] = $user_data->user_state;
 							$final['note'] = 'Nenhuma sugestão encontrada';
 							$this->response($final, REST_Controller::HTTP_OK);
-						// }
+						}
 
 						
 
