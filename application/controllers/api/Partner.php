70600683e4cfc24393075283f047a74a961e461d
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-05-28 23:33:09 -0300

    ;

--- application/controllers/api/Partner.php
+++ application/controllers/api/Partner.php
@@ -257,7 +257,7 @@ class Partner extends REST_Controller
         $this->form_validation->set_rules('partner_duration', 'partner_duration', 'trim|required');
         $this->form_validation->set_rules('partnet_duration_type', 'partnet_duration_type', 'trim|required');
         $this->form_validation->set_rules('partner_status', 'partner_status', 'trim|required');
-        $this->form_validation->set_rules('is_deleted', 'is_deleted', 'trim|required');
+        // $this->form_validation->set_rules('is_deleted', 'is_deleted', 'trim|required');
 
 
         if ($this->form_validation->run() === false) {
