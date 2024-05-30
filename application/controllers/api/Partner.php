65eac381df50fd5dbe4fc36c66c4f6d80e247860
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-05-29 22:37:25 -0300

    .

--- application/controllers/api/Partner.php
+++ application/controllers/api/Partner.php
@@ -283,8 +283,8 @@ class Partner extends REST_Controller
                 $response['partner_data'] = $partner_data;
                 $response['owner_data'] = $this->user_model->get_user($partner_data->partner_property_owner);
                 $response['offer_data'] = $this->user_model->get_user($partner_data->partner_property_broker);
-                $response['property_data'] =  $this->partner_model->get_partner_associated($partner_id);
-                $response['partner_actions'] = $this->partner_model->get_partner_actions($partner_id);
+                $response['property_data'] = (array) $this->partner_model->get_partner_associated($partner_id);
+                $response['partner_actions'] = (array) $this->partner_model->get_partner_actions($partner_id);
 
 
                 $final['status'] = true;
