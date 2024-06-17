075fcced279b7e17ba3b3b57e1fffda96b214c96
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-06-17 12:38:24 -0300

    .

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -1703,6 +1703,8 @@ class User extends REST_Controller
 								$response[] = $response_a;
 							}
 
+							print_r($response); 
+							
 							$final['status'] = true;
 							$final['message'] = 'Posts encontrados com sucesso.';
 							$final['response'] = $response;
