744d132215acbdcdc78ff8d23fe68b85da7ee9c3
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-06-16 22:40:22 -0300

    .

--- application/controllers/api/Propertys.php
+++ application/controllers/api/Propertys.php
@@ -1651,28 +1651,28 @@ class Propertys extends REST_Controller
 
         // Verificar tipo de propriedade
         if (strlen($f_data['property_type']) > 0) {
-            if ($bp->property_type != $f_data['property_type']) {
+            if ($bp['property_type'] != $f_data['property_type']) {
                 $passes_filter = false;
             }
         }
 
         // Verificar tipo de oferta
         if (strlen($f_data['property_type_offer']) > 0) {
-            if ($bp->property_type_offer != $f_data['property_type_offer']) {
+            if ($bp['property_type_offer']  != $f_data['property_type_offer']) {
                 $passes_filter = false;
             }
         }
 
         // Verificar preço mínimo
         if (strlen($f_data['filter_price_min']) > 0) {
-            if ($bp->property_price < $f_data['filter_price_min']) {
+            if ($bp['property_price']  < $f_data['filter_price_min']) {
                 $passes_filter = false;
             }
         }
 
         // Verificar preço máximo
         if (strlen($f_data['filter_price_max']) > 0) {
-            if ($bp->property_price > $f_data['filter_price_max']) {
+            if ($bp['property_price']  > $f_data['filter_price_max']) {
                 $passes_filter = false;
             }
         }
