79e2c2680a55ee109c4143b71dc5fa0ffea14596
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-06-30 21:47:26 -0300

    .

--- application/controllers/api/Propertys.php
+++ application/controllers/api/Propertys.php
@@ -18,7 +18,79 @@ class Propertys extends REST_Controller
         $this->load->model('property_model');
     }
 
-  
+    // public function broker_propriety_post()
+    // {
+    //     $this->form_validation->set_rules('user_id', 'User ID', 'trim|required');
+
+    //     if ($this->form_validation->run() == false) {
+
+    //         $final['status'] = false;
+    //         $final['message'] = validation_errors();
+    //         $final['note'] = 'Erro no formulário.';
+
+    //         $this->response($final, REST_Controller::HTTP_OK);
+    //     } else {
+
+    //         $headers = $this->input->request_headers();
+
+    //         if (isset($headers['Authorization'])) {
+
+    //             $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+
+    //             if ($decodedToken['status']) {
+
+    //                 $user_id = $this->input->post('user_id');
+
+    //                 $_total_broker_propertys = array();
+    //                 $_broker_propertys =  $this->broker_model->get_broker_propertys($user_id);
+
+    //                 foreach ($_broker_propertys as $c) {
+
+    //                     if ($this->partner_model->check_exist_partner($c->id, $user_id)) {
+
+    //                         $c->check_partner = true;
+    //                         $_total_broker_propertys[] = $c;
+
+    //                     } else {
+
+    //                         $c->check_partner = false;
+    //                         $_total_broker_propertys[] = $c;
+    //                     }
+    //                 }
+
+    //                 if ($_broker_propertys) {
+
+    //                     $final['status'] = true;
+    //                     $final['message'] = 'Imóveis encontradas com sucesso.';
+    //                     $final['response'] = $_total_broker_propertys;
+    //                     $final['note'] = 'Dados   encontrados get_broker_propertys()';
+
+    //                     $this->response($final, REST_Controller::HTTP_OK);
+    //                 } else {
+
+    //                     $final['status'] = false;
+    //                     $final['message'] = 'Nenhum imoveil encontrado.';
+    //                     $final['note'] = 'Erro em get_broker_propertys()';
+
+    //                     $this->response($final, REST_Controller::HTTP_OK);
+    //                 }
+    //             } else {
+
+    //                 $final['status'] = false;
+    //                 $final['message'] = 'Sua sessão expirou.';
+    //                 $final['note'] = 'Erro em $decodedToken["status"]';
+    //                 $this->response($decodedToken);
+    //             }
+    //         } else {
+
+    //             $final['status'] = false;
+    //             $final['message'] = 'Falha na autenticação.';
+    //             $final['note'] = 'Erro em validateToken()';
+
+    //             $this->response($final, REST_Controller::HTTP_OK);
+    //         }
+    //     }
+    // }
 
     public function broker_propriety_post()
     {
@@ -42,7 +114,6 @@ class Propertys extends REST_Controller
                 if ($decodedToken['status']) {
 
                     $user_id = $this->input->post('user_id');
-                    
                     $_broker_propertys =  $this->broker_model->get_broker_propertys($user_id);
 
                     if ($_broker_propertys) {
