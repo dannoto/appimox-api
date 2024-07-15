2cfeaf4930b10ee0864b0be6d0af54379ffa7893
Daniel Ribeiro <sdanrib2018@gmail.com>
2024-07-15 15:43:49 -0300

    dfkjsdh

--- index.php
+++ index.php
@@ -66,12 +66,12 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
+		// error_reporting(-1);
+		// ini_set('display_errors', 1);
 		
 
-		// error_reporting(0);
-		// ini_set('display_errors', 0);
+		error_reporting(0);
+		ini_set('display_errors', 0);
 	break;
 
 	case 'testing':
